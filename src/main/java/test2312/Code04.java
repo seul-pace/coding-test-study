ca88e2ea4a882c534aafb0f4cfc9057250942acb
seulhui <lsh9412@naver.com>
2023-12-04 12:14:11 +0900

    명예의 전당
    - priorityQueue 이용

--- src/main/java/test2312/Code04.java
+++ src/main/java/test2312/Code04.java
@@ -2,6 +2,7 @@ package test2312;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * 명예의 전당
@@ -16,16 +17,38 @@ public class Code04 { // [10, 10, 10, 20, 20, 100, 100]
         }
     }
 
+//    public static int[] solution(int k, int[] score) {
+//        int[] answer = new int[score.length];
+//
+//        ArrayList<Integer> array = new ArrayList<>();
+//
+//        for (int i = 0; i < score.length; i++) {
+//            array.add(score[i]);
+//            array.sort(Comparator.reverseOrder());
+//
+//            answer[i] = i < k - 1 ? array.get(i) : array.get(k - 1);
+//        }
+//
+//        return answer;
+//    }
+
+    // 딱 알맞은 PriorityQueue가 있다고 해서 바꿔보기
     public static int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
 
-        ArrayList<Integer> array = new ArrayList<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int i = 0; i < score.length; i++) {
-            array.add(score[i]);
-            array.sort(Comparator.reverseOrder());
+            // 일단 넣고
+            queue.add(score[i]);
 
-            answer[i] = i < k - 1 ? array.get(i) : array.get(k - 1);
+            // 만약 사이즈가 넘었으면 (어차피 우선순위 지정되어 있을 테니) 첫 번째 값 제거
+            if (queue.size() > k) {
+                queue.poll();
+            }
+
+            // 정답은 일단.. 첫 번째 값 제거하지 않고 반환만
+            answer[i] = queue.peek();
         }
 
         return answer;
