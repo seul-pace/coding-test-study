9c3c6cbe8fc7cb65f08e5c2a2717e32aff576c33
seulhui <p_seulhee.lee@lotte.net>
2023-12-12 12:16:08 +0900

    소수 찾기
    - 에라토스테네스의 체 이용

--- src/main/java/test2312/Code10.java
+++ src/main/java/test2312/Code10.java
@@ -7,8 +7,8 @@ package test2312;
 public class Code10 {
 
     public static void main(String[] args) {
-        int result1 = solution(10);
-        int result2 = solution(5);
+        int result1 = solution2(10);
+        int result2 = solution2(5);
 
         System.out.println("result1 = " + result1);
         System.out.println("result2 = " + result2);
@@ -31,5 +31,41 @@ public class Code10 {
             }
         }
         return true;
+    }
+
+    // 에라토스테네서의 체 방식 이용
+    public static int solution2(int n) {
+        int answer = 0;
+
+        // n까지의 소수의 갯수를 구하는 문제?
+        // 배열로 n+1까지의 배열을 만들고,
+        // 1. 소수인 index들은 값이 모두 false가 될 것이다
+        // 2. 그러면 다 구한 다음에 false인 거 카운트만 하면 된다
+
+        boolean[] prime = new boolean[n + 1];
+        prime[0] = prime[1] = true;
+
+        // 2부터 루트n까지 배수들을 체크해준다
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            // 이미 체크된 거면 지나가기
+            if (prime[i]) {
+                continue;
+            }
+
+            // 배수들을 체크할 값의 다음 배수부터
+            // 계속 배수들을 체크해서 true 처리
+            for (int j = i*i; j < prime.length; j = j+i) {
+                prime[j] = true;
+            }
+        }
+
+        // 카운트 한다
+        for (int i = 0; i < prime.length; i++) {
+            if (!prime[i]) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
