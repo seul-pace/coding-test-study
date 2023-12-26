020f08f0d52c702cea6ea977f1339e72a92fa252
seulhui <lsh9412@naver.com>
2023-12-26 20:11:52 +0900

    몫 구하기

---
+++ src/main/java/test2312/Code26.java
@@ -0,0 +1,16 @@
+package test2312;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120805
+ * 몫 구하기
+ */
+public class Code26 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(10, 5));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
