85cda96ff90f9f9cc1e5bfadba11fc3204f1a773
seulhui <lsh9412@naver.com>
2025-06-13 11:20:25 +0900

    백준 1단계 - 조건문

--- src/main/java/backjoon2025/june/practice_if/Main.java
+++ src/main/java/backjoon2025/june/practice_if/Main.java
@@ -7,24 +7,107 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
+        String input = sc.nextLine();
+        String[] arr = input.split(" ");
 
-        if (x > 0) {
-            if (y > 0) {
-                System.out.println("1");
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(arr[2]);
+
+        int prize = 0;
+
+        if (a == b && b == c) {
+            // 3개 다 같은 수
+            prize = 10_000 + a * 1_000;
+        } else if (a == b || b == c || a == c) {
+            // 2개만 같은 수
+            if (a == b || b == c) {
+                prize = 1_000 + b * 100;
             } else {
-                System.out.println("4");
+                prize = 1_000 + c * 100;
             }
         } else {
-            if (y > 0) {
-                System.out.println("2");
-            } else {
-                System.out.println("3");
-            }
+            // 전부 다른 수
+            int max = Math.max(a, b);
+            max = Math.max(max, c);
+            prize = max * 100;
         }
+
+        System.out.println(prize);
     }
 }
+
+/*
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputTime = sc.nextLine();
+        int inputCost = sc.nextInt();
+
+        // 시간 -> 분
+        String[] arr = inputTime.split(" ");
+        int hour = Integer.parseInt(arr[0]);
+        int minute = Integer.parseInt(arr[1]);
+
+        int total = hour * 60 + minute + inputCost;
+
+        // 시간/분 변환
+        int h = total / 60;
+        int m = total % 60;
+
+        if (h >= 24) {
+            h = h - 24;
+        }
+
+        System.out.println(h + " " + m);
+    }
+ */
+
+/*
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        String[] arr = input.split(" ");
+
+        int h = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+
+        // 45분 빼기
+        int minute = m - 45;
+        int hour = h;
+        if (minute < 0) {
+            // 이전 시간 가져오기
+            hour = h - 1;
+            if (hour < 0) {
+                hour = 23;
+            }
+            // 분 조정
+            minute += 60;
+        }
+        System.out.println(hour + " " + minute);
+    }
+ */
+
+/*public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+
+    if (x > 0) {
+        if (y > 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("4");
+        }
+    } else {
+        if (y > 0) {
+            System.out.println("2");
+        } else {
+            System.out.println("3");
+        }
+    }
+}*/
 
 /*public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
