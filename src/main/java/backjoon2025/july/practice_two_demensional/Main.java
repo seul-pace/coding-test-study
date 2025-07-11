f3cd14a102302070ca4180d1dfe66bdacf0e12ac
seulhui <lsh9412@naver.com>
2025-07-11 09:54:05 +0900

    백준 - 2차원 배열

--- src/main/java/backjoon2025/july/practice_two_demensional/Main.java
+++ src/main/java/backjoon2025/july/practice_two_demensional/Main.java
@@ -13,30 +13,86 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        int max = 0;
-        int a = 1, b = 1;
 
-        for(int i = 0; i < 9; i++) {
+        int[][] paper = new int[100][100];
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 9; j++) {
-                int n = Integer.parseInt(st.nextToken());
-                if (max < n) {
-                    max = n;
-                    a = i + 1;
-                    b = j + 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for (int j = a; j < a + 10; j++) {
+                for (int k = b; k < b + 10; k++) {
+                    paper[j][k] = 1;
                 }
             }
         }
 
-        bw.write(String.valueOf(max));
-        bw.newLine();
-        bw.write(a + " " + b);
-        bw.flush();
-        bw.close();
+        int result = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (paper[i][j] == 1) result++;
+            }
+        }
+
+        System.out.println(result);
     }
 }
 
 /**
+ * public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         int index;
+ *         String[][] result = new String[5][15];
+ *
+ *         // 입력
+ *         for(int i = 0; i < 5; i++) {
+ *             index = 0;
+ *             String input = br.readLine();
+ *             for(int j = 0; j < input.length(); j++) {
+ *                 result[i][index++] = Character.toString(input.charAt(j));
+ *             }
+ *         }
+ *         // 출력
+ *         for (int i = 0; i < 15; i++) {
+ *             for (int j = 0; j < 5; j++) {
+ *                 if (result[j][i] != null) {
+ *                     bw.write(String.valueOf(result[j][i]));
+ *                 }
+ *             }
+ *         }
+ *         bw.flush();
+ *         bw.close();
+ *     }
+ *
+ * public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *         StringTokenizer st;
+ *         int max = 0;
+ *         int a = 1, b = 1;
+ *
+ *         for(int i = 0; i < 9; i++) {
+ *             st = new StringTokenizer(br.readLine());
+ *             for(int j = 0; j < 9; j++) {
+ *                 int n = Integer.parseInt(st.nextToken());
+ *                 if (max < n) {
+ *                     max = n;
+ *                     a = i + 1;
+ *                     b = j + 1;
+ *                 }
+ *             }
+ *         }
+ *
+ *         bw.write(String.valueOf(max));
+ *         bw.newLine();
+ *         bw.write(a + " " + b);
+ *         bw.flush();
+ *         bw.close();
+ *     }
+ *
  * public static void main(String[] args) throws IOException {
  *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
