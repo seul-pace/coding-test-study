c41a41be72544cfdfc8c413d4da65c4266562054
seulhui <lsh9412@naver.com>
2025-07-22 21:31:32 +0900

    백준 - 배수와 약수

--- src/main/java/backjoon2025/july/practice_decimal/Main.java
+++ src/main/java/backjoon2025/july/practice_decimal/Main.java
@@ -11,42 +11,71 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String factor = "factor";
-        String multiple = "multiple";
-        String neither = "neither";
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st;
-        while (true) {
-            st = new StringTokenizer(br.readLine());
+        int count = 0;
+        int aliquot = 0;
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
 
-            if (a == 0 && b == 0) {
-                break;
-            }
-
-            // 로직 on . . .
-            if (a > b) {
-                if (a % b == 0) {
-                    bw.write(multiple);
-                } else {
-                    bw.write(neither);
-                }
-            } else {
-                if (b % a == 0) {
-                    bw.write(factor);
-                } else {
-                    bw.write(neither);
+                if (count == k) {
+                    aliquot = i;
+                    break;
                 }
             }
-            bw.newLine();
         }
 
-        br.close();
-        bw.flush();
-        bw.close();
+        if (count < k) {
+            aliquot = 0;
+        }
+        System.out.println(aliquot);
     }
 }
+
+/**
+ * public static void main(String[] args) throws IOException {
+ *         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ *         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+ *
+ *         String factor = "factor";
+ *         String multiple = "multiple";
+ *         String neither = "neither";
+ *
+ *         StringTokenizer st;
+ *         while (true) {
+ *             st = new StringTokenizer(br.readLine());
+ *
+ *             int a = Integer.parseInt(st.nextToken());
+ *             int b = Integer.parseInt(st.nextToken());
+ *
+ *             if (a == 0 && b == 0) {
+ *                 break;
+ *             }
+ *
+ *             // 로직 on . . .
+ *             if (a > b) {
+ *                 if (a % b == 0) {
+ *                     bw.write(multiple);
+ *                 } else {
+ *                     bw.write(neither);
+ *                 }
+ *             } else {
+ *                 if (b % a == 0) {
+ *                     bw.write(factor);
+ *                 } else {
+ *                     bw.write(neither);
+ *                 }
+ *             }
+ *             bw.newLine();
+ *         }
+ *
+ *         br.close();
+ *         bw.flush();
+ *         bw.close();
+ *     }
+ */
