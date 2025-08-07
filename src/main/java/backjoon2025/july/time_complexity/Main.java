dfc5f7c6ff4524f668ab05387c45078bf43385b4
seulhui <lsh9412@naver.com>
2025-08-07 09:39:59 +0900

    백준 - 시간 복잡도

--- src/main/java/backjoon2025/july/time_complexity/Main.java
+++ src/main/java/backjoon2025/july/time_complexity/Main.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    // 24263: 특정 알고리즘의 시간 복잡도 구하기 문제
+    // 24264: 특정 알고리즘의 시간 복잡도 구하기 문제
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.next());
-        System.out.println(1);
+        long l = Long.parseLong(sc.next());
+        System.out.println(l * l); // 반복문이 중첩되어 n 제곱 만큼 실행 (=수행 횟수)
+        System.out.println(2); // 제곱이니까 2
     }
 }
 
 /**
+ *
+ * // 24263: 특정 알고리즘의 시간 복잡도 구하기 문제
+ *     public static void main(String[] args) {
+ *         Scanner sc = new Scanner(System.in);
+ *         System.out.println(sc.next()); // 들어오는 만큼 반복문 진행
+ *         System.out.println(1); // 차수,,
+ *     }
+ *
  * // 24262: 특정 알고리즘의 시간 복잡도 구하기 문제
  *     public static void main(String[] args) {
  *         System.out.println(1); // 어떤 값이 들어가든 1번만 수행해서 횟수는 1
