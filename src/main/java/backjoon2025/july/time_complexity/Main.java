3e2a3fcc530fb122c2fef615b34ea0057897dc3a
seulhui <lsh9412@naver.com>
2025-08-11 10:59:17 +0900

    백준 - 시간 복잡도

--- src/main/java/backjoon2025/july/time_complexity/Main.java
+++ src/main/java/backjoon2025/july/time_complexity/Main.java
@@ -1,10 +1,26 @@
 package backjoon2025.july.time_complexity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    // 24313:
-    public static void main(String[] args) {
+    // 24313: 사실 무슨 소린지 모르겠음
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
 
+        if (a1 * n0 + a0 <= c * n0 && a1 <= c) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
 }
 
